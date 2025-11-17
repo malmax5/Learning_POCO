12bab07367b2fd21081420de04461dfc31409e5f
cbf <malmax5@mail.ru>
2025-11-17 15:52:08 +0300

    add: LRUCache work demonstration

--- 140-Cache/POCO_LRUCache/main.cpp
+++ 140-Cache/POCO_LRUCache/main.cpp
@@ -1,6 +1,29 @@
+#include "Poco/LRUCache.h"
 #include <iostream>
+
+using Poco::LRUCache;
+using namespace std;
+
+void lrucache_demo()
+{
+    LRUCache<int, string> cache(3);
+
+    cache.add(1, "Apple");
+    cache.add(2, "Banana");
+    cache.add(3, "Cherry");
+
+    cout << "Size after adding 3 elements: " << cache.size() << endl; // 3
+
+    cache.add(4, "Date"); // Вытеснит "Apple" (самый старый)
+
+    auto item = cache.get(1);
+    if (item.isNull())
+    {
+        cout << "Key 1 was evicted" << endl;
+    }
+}
 
 int main()
 {
-    
+    lrucache_demo();
 }
