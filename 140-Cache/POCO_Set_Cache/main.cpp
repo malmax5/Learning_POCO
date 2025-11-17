ed49502c4fe075e3bd331dcc509ab6a65f21e60d
cbf <malmax5@mail.ru>
2025-11-17 16:14:45 +0300

    add: chache like set not map

--- 140-Cache/POCO_Set_Cache/main.cpp
+++ 140-Cache/POCO_Set_Cache/main.cpp
@@ -1,6 +1,25 @@
+#include "Poco/LRUCache.h"
+#include "Poco/Void.h"
 #include <iostream>
+
+using Poco::LRUCache;
+using Poco::Void;
+using namespace std;
+
+void cache_set_demo()
+{
+    LRUCache<string, Void> setCache(100);
+
+    setCache.add("user1", Void());
+    setCache.add("user2", Void());
+
+    if (setCache.has("user1"))
+    {
+        cout << "user1 exists" << endl;
+    }
+}
 
 int main()
 {
-    
+    cache_set_demo();
 }
